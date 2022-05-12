3078bedb5eb0ed3403bf24c49ed371d452b29be4
susi189 <ssteinhoff82@gmail.com>
2022-05-12 15:55:11 -0400

    Setting up authorization and tokens

--- src/handlers/product_routes.ts
+++ src/handlers/product_routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Product, ProductStore } from "../models/product";
+import jwt from "jsonwebtoken";
 
 const store = new ProductStore();
 
@@ -9,6 +10,16 @@ const index = async (req: express.Request, res: express.Response) => {
 };
 
 const create = async (req: express.Request, res: express.Response) => {
+  try {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
+  } catch (err) {
+    res.status(401);
+    res.json("Access denied, invalid token");
+    return;
+  }
+
   try {
     const product: Product = {
       name: req.body.name,
--- src/handlers/user_routes.ts
+++ src/handlers/user_routes.ts
@@ -1,23 +1,39 @@
 import express from "express";
 import { User, UserStore } from "../models/user";
+import jwt from "jsonwebtoken";
 
 const store = new UserStore();
 
 const index = async (req: express.Request, res: express.Response) => {
-  const users = await store.index();
-  res.json(users);
+  try {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
+  } catch (err) {
+    res.status(401);
+    res.json("Access denied, invalid token");
+    return;
+  }
+
+  try {
+    const users = await store.index();
+    res.json(users);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
 const create = async (req: express.Request, res: express.Response) => {
+  const user: User = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    passworddigest: req.body.passworddigest,
+  };
   try {
-    const user: User = {
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      passworddigest: req.body.passworddigest,
-    };
-
     const newUser = await store.create(user);
-    res.json(newUser);
+    let token = jwt.sign({ user: newUser }, process.env.TOKEN_SECRET as string);
+    res.json(token);
   } catch (err) {
     res.status(400);
     res.json(err);
@@ -25,8 +41,23 @@ const create = async (req: express.Request, res: express.Response) => {
 };
 
 const show = async (req: express.Request, res: express.Response) => {
-  const user = await store.show(req.body.id);
-  res.json(user);
+  try {
+    const authorizationHeader = req.headers.authorization as string;
+    const token = authorizationHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
+  } catch (err) {
+    res.status(401);
+    res.json("Access denied, invalid token");
+    return;
+  }
+
+  try {
+    const user = await store.show(req.body.id);
+    res.json(user);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
 const product_routes = (app: express.Application) => {
