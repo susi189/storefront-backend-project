3c1c997f252f03ee146b53fb5f4c82c785c8530a
susi189 <ssteinhoff82@gmail.com>
2022-05-13 16:06:02 -0400

    Updated Jasmine spec reporter

--- src/models/tests/order_spec.ts
+++ src/models/tests/order_spec.ts
@@ -3,16 +3,19 @@ import { Order, OrderStore } from "../order";
 const store = new OrderStore();
 
 describe("Test for Order Model", () => {
-  it("should have a show method", () => {
-    expect(store.show).toBeDefined();
+  it("should have a create method", () => {
+    expect(store.create).toBeDefined();
   });
 
-  it("show method should return a product with given id", async () => {
-    const result = await store.show(1);
+  it("create method should insert a new order into the orders table", async () => {
+    const result = await store.create({
+      status: "active",
+      uid: 2,
+    });
     expect(result).toEqual({
       id: 1,
-      name: "my product",
-      price: "1.99",
+      status: "active",
+      uid: 2,
     });
   });
 });
